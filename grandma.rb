6dd01cf167d52f16d26830f8459b2c0b541f332b
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-14 15:31:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
-def speak_to_grandma(string)
-  if string == string.upcase
-    return "NO, NOT SINCE 1938!"
-  elsif string == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+def speak_to_grandma(greeting)
+  if (greeting == greeting.upcase)
+    'NO, NOT SINCE 1938!'
+  elsif (greeting == 'I LOVE YOU GRANDMA!')
+    'I LOVE YOU TOO PUMPKIN!'
   else
-    return "HUH?! SPEAK UP, SONNY!"
+    'HUH?! SPEAK UP, SONNY!'
 end
